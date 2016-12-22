63e6a07bc1b4e414c3612a5d071921831d78ebe9
lalbert <lucalbert66@gmail.com>
2016-12-22 11:42:13 +0100

    Implements NGramTokenFilter

---
+++ src/TokenFilter/NGramTokenFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace TokenizerEngine\TokenFilter;
+
+class NGramTokenFilter implements TokenFilterInterface
+{
+    private $minGram = 1;
+    private $maxGram = 2;
+    private $preserveOriginal = true;
+
+    public function __construct($minGram = 1, $maxGram = 2, $preserveOriginal = true)
+    {
+        $this->minGram = (int) $minGram;
+        $this->maxGram = (int) $maxGram;
+        $this->preserveOriginal = (bool) $preserveOriginal;
+
+        if ($this->minGram < 1 || $this->maxGram < 1) {
+            throw new \InvalidArgumentException('minGram and maxGram must be greater than 0.');
+        }
+
+        if ($this->minGram >= $this->maxGram) {
+            throw new \InvalidArgumentException('maxGram must be greater than minGram.');
+        }
+    }
+
+    public function filter(array $tokens)
+    {
+        $_tokens = [];
+
+        foreach ((array) $tokens as $token) {
+            $_tokens = array_merge($_tokens, [$token], $this->nGram($token));
+        }
+
+        return $_tokens;
+    }
+
+    private function nGram($token)
+    {
+        $nGrams = [];
+        $strLen = mb_strlen($token);
+
+        for ($start = 0; $start < $strLen; ++$start) {
+            for ($length = $this->minGram; $length <= $this->maxGram; ++$length) {
+                $nGram = mb_strcut($token, $start, $length);
+                if ($length == mb_strlen($nGram)) {
+                    $nGrams[] = $nGram;
+                }
+            }
+        }
+
+        return $nGrams;
+    }
+}
---
+++ tests/Unit/TokenFilter/NGramTokenFilterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace TokenizerEngine\Tests\Unit\TokenFilter;
+
+use TokenizerEngine\TokenFilter\NGramTokenFilter;
+
+class NGramTokenFilterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFilter()
+    {
+        $tokens = ['Hello', 'world !'];
+        $expected = ['Hello', 'H', 'He', 'e', 'el', 'l', 'll', 'l', 'lo', 'o', 'world !', 'w', 'wo', 'o', 'or', 'r', 'rl', 'l', 'ld', 'd', 'd ', ' ', ' !', '!'];
+        $filter = new NGramTokenFilter();
+
+        $this->assertEquals($expected, array_values($filter->filter($tokens)));
+    }
+    
+    public function testMinMaxNgrams()
+    {
+        $tokens = ['Hello', 'world !'];
+        $expected = ['Hello', 'He', 'Hel', 'Hell', 'el', 'ell', 'ello', 'll', 'llo', 'lo', 'world !', 'wo', 'wor', 'worl', 'or', 'orl', 'orld', 'rl', 'rld', 'rld ', 'ld', 'ld ', 'ld !', 'd ', 'd !', ' !'];
+        $filter = new NGramTokenFilter(2, 4);
+        
+        $this->assertEquals($expected, array_values($filter->filter($tokens)));
+    }
+    
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage minGram and maxGram must be greater than 0.
+     */
+    public function testBadMinMaxValue()
+    {
+        $filter = new NGramTokenFilter(0, 0);
+    }
+    
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage maxGram must be greater than minGram.
+     */
+    public function testMinGreaterThanMax()
+    {
+        $filter = new NGramTokenFilter(2, 1);
+    }
+}
